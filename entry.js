eaff63e6793d82cd4367c5a2a03603e6e4a8a548
Paul Bevis <paulbevis2010@gmail.com>
2016-04-09 18:09:06 +1000

    Initial checkin

---
+++ entry.js
@@ -0,0 +1,80 @@
+/*
+ Copyright 2016 Paul Bevis
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import Spelling from './js/containers/spelling'
+
+
+import { createStore } from 'redux'
+import spellingAppReducers from './js/reducers/spelling'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin();
+
+const store = createStore(spellingAppReducers);
+
+render(
+  <Provider store={store}>
+    <Spelling />
+  </Provider>,
+  document.getElementById('framework')
+)
+
+if (typeof Object.assign != 'function') {
+    (function () {
+        Object.assign = function (target) {
+            'use strict';
+            if (target === undefined || target === null) {
+                throw new TypeError('Cannot convert undefined or null to object');
+            }
+
+            var output = Object(target);
+            for (var index = 1; index < arguments.length; index++) {
+                var source = arguments[index];
+                if (source !== undefined && source !== null) {
+                    for (var nextKey in source) {
+                        if (source.hasOwnProperty(nextKey)) {
+                            output[nextKey] = source[nextKey];
+                        }
+                    }
+                }
+            }
+            return output;
+        };
+    })();
+}
+if (!Array.prototype.find) {
+    Array.prototype.find = function(predicate) {
+        if (this === null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
