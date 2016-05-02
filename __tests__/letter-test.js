a2d70d60ba57eab89eb06de198b504c4836b0101
Paul Bevis <paulbevis2010@gmail.com>
2016-05-02 13:43:13 +1000

    Jest tests

---
+++ __tests__/letter-test.js
@@ -0,0 +1,71 @@
+'use strict';
+
+jest.unmock('../js/components/letter');
+jest.unmock('material-ui/lib/raised-button');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import Letter from '../js/components/letter';
+
+describe('Letter tests', () => {
+  it('if not disabled, then it allows clicks, sending the name', () => {
+    let buttonName = 'a';
+
+    const myFunc = ((name) => {
+      expect(buttonName).toEqual(name)
+    });
+
+    // Render a letter component in the document
+    const letter = TestUtils.renderIntoDocument(
+      <Letter status='Waiting For Input' name="a" onLetterClicked={myFunc}/>
+    );
+
+    const letterNode = ReactDOM.findDOMNode(letter);
+    let button = letterNode.children[0];
+    TestUtils.Simulate.click(button);
+    const spanNode=button.children[0].children[0];
+    expect(spanNode.textContent).toEqual('a');
+  });
+
+  it('if not disabled, then it allows clicks, fails if wrong name', () => {
+    let buttonName = 'a';
+
+    const myFunc = ((name) => {
+      expect('wrong name').not.toEqual(name)
+    });
+
+    // Render a checkbox with label in the document
+    const letter = TestUtils.renderIntoDocument(
+      <Letter status='Waiting For Input' name="a" onLetterClicked={myFunc}/>
+    );
+
+    const letterNode = ReactDOM.findDOMNode(letter);
+    let button = letterNode.children[0];
+    expect(button.style.color).toEqual('rgb(3, 3, 3)')
+    TestUtils.Simulate.click(button);
+    const spanNode=button.children[0].children[0];
+    expect(spanNode.textContent).toEqual('a');
+  });
+
+  it('button disabled, so no click allowed', () => {
+    let buttonName = 'a';
+    const mockFn = jest.fn();
+
+    // Render a checkbox with label in the document
+    const letter = TestUtils.renderIntoDocument(
+      <Letter status='Will make button disabled!' name="a" onLetterClicked={mockFn}/>
+    );
+
+    const letterNode = ReactDOM.findDOMNode(letter);
+    let button = letterNode.children[0];
+    TestUtils.Simulate.click(button);
+    expect(mockFn).not.toBeCalled();
+    console.log('button style:',button.style)
+    expect(button.style.color).toEqual('rgb(127, 127, 127)')
+    const spanNode=button.children[0].children[0];
+    expect(spanNode.textContent).toEqual('a');
+
+  });
+
+});
