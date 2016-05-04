48abd28a1394dc03372534a9eaf5f07552c2fbc4
Paul Bevis <paulbevis2010@gmail.com>
2016-05-05 07:49:37 +1000

    no longer pass the sound object task value

--- js/actions/spelling.js
+++ js/actions/spelling.js
@@ -23,10 +23,9 @@ export function startGameAction() {
     type: GAME_START
   }
 }
-export function finishedPlayingSoundAction(value) {
+export function finishedPlayingSoundAction() {
   return {
-    type: FINISHED_PLAYING_SOUND,
-    value
+    type: FINISHED_PLAYING_SOUND
   }
 }
 export function letterClickedAction(letter) {
