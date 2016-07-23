78005ba18b1c4a0b5145e329004e5d02eec64b86
Paul Bevis <paulbevis2010@gmail.com>
2016-07-23 16:54:48 +1000

    animation of the letter when clicked

--- js/components/letter.js
+++ js/components/letter.js
@@ -18,16 +18,58 @@
 import React, {PropTypes, Component} from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import {WAITING_FOR_INPUT} from '../constants/data';
+import {Motion, spring} from 'react-motion';
 
-let myStyle = {minWidth: '47px', margin: '5px 2px 5px 5px'};
-const disabledColour = '#3A938C';
-const Letter = (props) => <RaisedButton label={props.letter}
-                                        disabled={!(props.status === WAITING_FOR_INPUT)}
-                                        disabledBackgroundColor={disabledColour}
-                                        backgroundColor={disabledColour}
-                                        style={myStyle}
-                                        onClick={() => props.onLetterClicked(props.letter)}
-                                        labelStyle={{textTransform: 'lowercase',fontSize: '18px'}}/>;
+// const disabledColour = '#3A938C';
+class Letter extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false, buttonPressed: false};
+    this.onRest = this.onRest.bind(this);
+  }
+
+  onRest() {
+    setTimeout(() => {
+      if (this.state.buttonPressed) {
+        this.setState({open: !this.state.open, buttonPressed: false});
+      }
+    }, 0);
+  }
+
+  // componentWillUpdate(nextProps, nextState) {
+  //   console.log('componentWillUpdate');
+  // }
+
+  render() {
+    return (
+      <Motion style={{
+        x: spring(this.state.open ? 400 : 0, {stiffness: 40, damping: 8}),
+        y: spring(this.state.open ? 360 : 0, {stiffness: 50, damping: 10}),
+        z: spring(this.state.open ? 10 : 0)
+      }} onRest={this.onRest}>
+        {({x, y, z}) =>
+          <RaisedButton label={this.props.letter}  primary={true}
+                        disabled={!(this.props.status === WAITING_FOR_INPUT)}
+
+                        style={{
+                          transform: `perspective(30px) translate3d(${x}px, 0, ${z}px) rotate(${y}deg)`,
+                          minWidth: '47px',
+                          margin: '5px 2px 5px 5px'
+                        }}
+                        onClick={() => this.buttonClicked()}
+                        ref={(ref) => this.myRef = ref}
+                        labelStyle={{textTransform: 'lowercase',fontSize: '18px'}}/>
+        }
+      </Motion>);
+  }
+
+
+  buttonClicked() {
+    this.setState({open: !this.state.open, buttonPressed: !this.state.buttonPressed});
+    this.props.onLetterClicked(this.props.letter);
+  }
+}
 
 Letter.propTypes = {
   letter: PropTypes.string.isRequired,
